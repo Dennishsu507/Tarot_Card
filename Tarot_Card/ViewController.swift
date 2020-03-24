fe4bf6c11807a9b97d3c2a5bc77dc553d190b060
hongweihsu <dennishsu@arch.nctu.edu.tw>
2020-03-24 15:48:30 +0800

    Initial Commit

---
+++ Tarot_Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tarot_Card
+//
+//  Created by HSU, Hong-Wei on 2020/3/24.
+//  Copyright © 2020 HSU, Hong-Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
